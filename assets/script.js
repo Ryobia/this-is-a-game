609d84c3b402ef60df38515faeb6f400a35ec957
Ryobia <ryobia36@gmail.com>
2021-03-02 15:45:59 -0700

    ITS A GAME NOW! the runner chases you and console logs a message when he catches you, ending the game

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ let handi = $('#handi');
 let baddie = $('#baddie');
 let pos = '0px';
 let speed = 10;
-let baddieSpeed = 5;
+let baddieSpeed = 10;
 let handiX = handi.offset();
 
 let baddieX = baddie.offset();
@@ -38,30 +38,45 @@ let backgroundJunk = setInterval(function() {
     console.log(handiX);
     console.log(baddieX);
     handiX = handi.offset();
+    baddieX = baddie.offset();
+    baddieGo(handiX, baddieX);
     
     if (hitbox(handi, baddie)) {
-        alert("He got you")
+        console.log("He got you");
+        clearInterval(backgroundJunk);
     }
     
 
 
-}, 3000);
+}, 100);
 
-let baddieGo = setInterval(function() {
+let baddieGo = function(handiX, baddieX) {
 
 
    
     if (baddieX.left < handiX.left) {
         console.log("left");
-        $(baddie).animate({left: '+=' + speed + 'px'}, 10);
+        $(baddie).animate({left: '+=' + baddieSpeed + 'px'}, 10);
 
-    } else if (baddieX.left > handiX.left) {
+    }
+    if (baddieX.left > handiX.left) {
         console.log("right");
-        $(baddie).animate({left: '-=' + speed + 'px'}, 10);
+        $(baddie).animate({left: '-=' + baddieSpeed + 'px'}, 10);
+
+    }
+    if (baddieX.top < handiX.top) {
+        console.log("left");
+        $(baddie).animate({bottom: '-=' + baddieSpeed + 'px'}, 10);
+        
+    }
+    if (baddieX.top > handiX.top) {
+        console.log("right");
+        $(baddie).animate({bottom: '+=' + baddieSpeed + 'px'}, 10);
     }
 
 
-}, 1000) ;
+
+};
 
 
 
@@ -95,8 +110,8 @@ document.addEventListener("keydown", function(e) {
 
 document.addEventListener("keydown", function(e) {
     if(e.key === 'd') {
-        $(handi).animate({left: '+=' + speed + 'px'}, 10);
-        // handi.style.left = `${parseInt(handi.style.left) + 5}px`;
+            $(handi).animate({left: '+=' + speed + 'px'}, 10);
+            // handi.style.left = `${parseInt(handi.style.left) + 5}px`;
         
 
     }

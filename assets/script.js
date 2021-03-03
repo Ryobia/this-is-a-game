47822637809b26c4e5fcf5e6c2136371d23c4426
Ryobia <ryobia36@gmail.com>
2021-03-02 19:05:00 -0700

    not much progress but learned lots

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 let handi = $('#handi');
 let baddie = $('#baddie');
-let pos = '0px';
+let field = $('#field');
 let speed = 10;
 let baddieSpeed = 10;
 let handiX = handi.offset();
@@ -33,18 +33,42 @@ let hitbox = function (handi, baddie) {
     return !(handiTop < baddieOffset.top || handiOffset.top > baddieTop || handiLeft < baddieOffset.left || handiOffset.left > baddieLeft);
 };
 
+let outOfBounds = function(handi, field) {
+
+    let handiOffset = handi.offset();
+    let handiHeight = handi.outerHeight(true);
+    let handiWidth = handi.outerWidth(true);
+    let handiTop = handiOffset.top + handiHeight;
+    let handiLeft = handiOffset.left + handiWidth;
+
+    let fieldOffset = field.offset();
+    let fieldHeight = field.outerHeight(true);
+    let fieldWidth = field.outerWidth(true);
+    let fieldTop = fieldOffset.top + fieldHeight;
+    let fieldLeft = fieldOffset.left + fieldWidth;
+
+    return !(handiTop < fieldOffset.top || handiOffset.top > fieldTop || handiLeft < fieldOffset.left || handiOffset.left > fieldLeft);
+
+}
+
 
 let backgroundJunk = setInterval(function() {
-    console.log(handiX);
-    console.log(baddieX);
     handiX = handi.offset();
     baddieX = baddie.offset();
     baddieGo(handiX, baddieX);
     
+    if (outOfBounds(handi, field)) {
+        console.log("in");
+    } else if (!outOfBounds(handi, field)) {
+        console.log("out")
+    }
+
     if (hitbox(handi, baddie)) {
-        console.log("He got you");
+        alert("He got you");
         clearInterval(backgroundJunk);
     }
+
+  
     
 
 
@@ -55,22 +79,18 @@ let baddieGo = function(handiX, baddieX) {
 
    
     if (baddieX.left < handiX.left) {
-        console.log("left");
         $(baddie).animate({left: '+=' + baddieSpeed + 'px'}, 10);
 
     }
     if (baddieX.left > handiX.left) {
-        console.log("right");
         $(baddie).animate({left: '-=' + baddieSpeed + 'px'}, 10);
 
     }
     if (baddieX.top < handiX.top) {
-        console.log("left");
         $(baddie).animate({bottom: '-=' + baddieSpeed + 'px'}, 10);
         
     }
     if (baddieX.top > handiX.top) {
-        console.log("right");
         $(baddie).animate({bottom: '+=' + baddieSpeed + 'px'}, 10);
     }
 
@@ -83,9 +103,9 @@ let baddieGo = function(handiX, baddieX) {
 document.addEventListener("keydown", function(e) {
     if(e.key === 'w') {
         
-        $(handi).animate({bottom: '+=' + speed + 'px'}, 10);
-        // handi.style.bottom = `${parseInt(handi.style.bottom) + 5}px`;
-
+            $(handi).animate({bottom: '+=' + speed + 'px'}, 10);
+            // handi.style.bottom = `${parseInt(handi.style.bottom) + 5}px`;
+       
 
     }
 });
@@ -93,6 +113,8 @@ document.addEventListener("keydown", function(e) {
 document.addEventListener("keydown", function(e) {
     if(e.key === 'a') {
         $(handi).animate({left: '-=' + speed + 'px'}, 10);
+        handiX = handi.offset();
+        console.log(handiX);
         // handi.style.left = `${parseInt(handi.style.left) - 5}px`;
 
 
@@ -112,6 +134,7 @@ document.addEventListener("keydown", function(e) {
     if(e.key === 'd') {
             $(handi).animate({left: '+=' + speed + 'px'}, 10);
             // handi.style.left = `${parseInt(handi.style.left) + 5}px`;
+            
         
 
     }
